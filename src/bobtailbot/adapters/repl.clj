48cfcac40402f6954a4d8e044ca74642d1cfbf46
martin <martinobal@gmail.com>
2019-02-12 19:51:14 +0100

    Minor edits to configs snippet. Use it in the repl adapter too

--- src/bobtailbot/adapters/repl.clj
+++ src/bobtailbot/adapters/repl.clj
@@ -1,6 +1,29 @@
 (ns bobtailbot.adapters.repl
+  (:require [outpace.config :refer [defconfig]])
   (:gen-class)
   )
+
+
+
+
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;;;;;;;; Adapter-specific chat config options. They override global options if "use-global?" is set to false. CLI options override all.
+;;;;;;;;; All configs can be set in the file "config.edn"
+(defconfig use-global? false) ; true: use global configs. false: override global configs.
+(defconfig nick "bobtailbot")
+(defconfig host "127.0.0.1")
+(defconfig port 6667)
+(defconfig group-or-chan nick) ; eg: "bobtailbot" ; if a prefix such as "#" is needed, the adapter must add it.
+(defconfig greeting "Hello.  Let's chat.")
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+
+
+
+
+
+
 
 (defn launch-repl [greeting respond-fn]
   (do
